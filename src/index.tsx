27bd2b8eb370f5d59cc8a9a1ceea9f9817001b9a
chiragmak10 <chiragmak100@gmail.com>
2021-07-11 13:45:23 +0530

    Added commit hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import { Mainpage } from "./views/Mainpage";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Mainpage />
   </React.StrictMode>,
   document.getElementById("root")
 );
